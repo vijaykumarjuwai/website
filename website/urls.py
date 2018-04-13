dadfd3bed41983dc042964c85e9d344f65de770f
Vijaykumar <vijaykumarbhattacharji@hotmail.com>
2018-04-13 17:05:19 +0800

    Updated with some contact details

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(
-        template_name='website/index.html'), name='index')
+    url(r'^$', TemplateView.as_view(template_name='website/index.html'), name='index'),
+    url(r'^contact$', TemplateView.as_view(template_name='website/contact.html'), name='contact'),
 ]
